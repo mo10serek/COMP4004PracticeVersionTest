3a5b5c3429068a9d8b564c323935edbfbc687d13
MichaelBalcerzak <balcerzak.michael@gmail.com>
2022-09-17 14:56:19 -0400

    updated main file

--- src/main/java/org/example/SquareRoot.java
+++ src/main/java/org/example/SquareRoot.java
@@ -1,13 +1,13 @@
 package org.example;
 
+import java.text.DecimalFormat;
+
 public class SquareRoot {
 
-    public void squareRoot() {
-
-    }
+    private static final DecimalFormat df = new DecimalFormat("0.00");
 
     public double caculateRoot(double input) {
         double output = Math.sqrt(input);
-        return output;
+        return (double) Math.round(output * 100) / 100;
     }
 }
